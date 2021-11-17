c29b3958ba9e90cb03d413f1a6744ef27471e297
Théo Arnauld des lions <arnaulddeslion@edu.univ-eiffel.fr>
2021-11-17 17:59:24 +0100

    Central Queue Start

---
+++ LibQueue/include/ObjQueue.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Object.hpp"
+
+class ObjQueue {
+private :
+    Object* _obj;
+    ObjQueue* _next;
+
+public :
+
+
+};
---
+++ LibQueue/include/Queue.hpp
@@ -0,0 +1,16 @@
+#pragma once 
+
+#include "ObjQueue.hpp"
+
+#define MAX_ELEMENTS_IN_QUEUE 10
+
+class Queue {
+private:
+    ObjQueue* _first;
+    ObjQueue* _last;
+    int total;
+
+public :
+
+
+};
